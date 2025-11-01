2f8daaba00049e6cada25902560eb640c9f3c2e8
Martin Löffler <me@fatalerrorcoded.eu>
2025-11-01 01:01:38 +0100

    refactor: generalize wireguard tunnel struct for both client and server

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@ pub mod tunnel;
 use config::Config;
 use futures::future::join_all;
 
+use crate::tunnel::{TunnelManager, TunnelPeer};
+
 #[tokio::main]
 async fn main() {
     let config_data = fs::read("./boringchain.toml").expect("No boringchain.toml config");
     let config: Config = toml::from_slice(&config_data).expect("Invalid config");
 
-    let server_task = tunnel::server::start_server_task(config.server);
+    let server_peers: Vec<_> = config.server.peers.iter().map(TunnelPeer::from).collect();
+    let server_task = TunnelManager::new(config.server.listen_port, config.server.private_key, &server_peers).run();
 
     join_all([server_task]).await;
 }
