c4a95149ddff58930b14a4a9b8aa31adf7cdb78c
Martin Löffler <me@fatalerrorcoded.eu>
2025-11-02 14:22:52 +0100

    refactor: switch from ingot to smoltcp
    ingot doesn't handle checksums, smoltcp does

--- src/nat/mod.rs
+++ src/nat/mod.rs
@@ -1,12 +1,10 @@
 use std::collections::HashMap;
 use std::net::{Ipv4Addr, SocketAddrV4};
 
-use bitflags::bitflags_match;
-use ingot::icmp::{IcmpV4Mut, IcmpV4Ref, IcmpV4Type, ValidIcmpV4};
-use ingot::ip::{IpProtocol, Ipv4Mut, Ipv4Ref, ValidIpv4};
-use ingot::tcp::{TcpFlags, TcpMut, TcpRef, ValidTcp};
-use ingot::types::HeaderParse;
-use ingot::udp::{UdpMut, UdpRef, ValidUdp};
+use smoltcp::wire::{
+    IPV4_HEADER_LEN, Icmpv4Message, Icmpv4Packet, IpProtocol, Ipv4Packet, TCP_HEADER_LEN,
+    TcpPacket, UDP_HEADER_LEN, UdpPacket,
+};
 use tokio::time::Instant;
 
 mod types;
@@ -31,16 +29,15 @@ impl AddressTranslator {
     }
 
     pub fn translate_outward(&mut self, data: &mut [u8]) -> Option<()> {
-        let (mut ipv4, _, rest) = ValidIpv4::parse(data).ok()?;
+        let mut ipv4 = Ipv4Packet::new_checked(data).ok()?;
+        let ip_source = ipv4.src_addr();
 
-        let ip_source = ipv4.source().into();
-        let key = Self::get_outward_key(ipv4.protocol(), rest, ip_source)?;
+        let key = Self::get_outward_key(ipv4.next_header(), ipv4.payload_mut(), ip_source)?;
         let entry = self.outward.get_mut(&key);
 
         if let Some(entry) = entry {
             let new_state = Self::translate_packet(
                 &mut ipv4,
-                rest,
                 Some(self.public_address),
                 None,
                 Some(entry.external_port),
@@ -56,11 +53,10 @@ impl AddressTranslator {
         let external_port = self.get_free_external_port(key.protocol, key.port)?;
         let protocol_state = Self::translate_packet(
             &mut ipv4,
-            rest,
             Some(self.public_address),
             None,
             Some(external_port),
-            None
+            None,
         )?;
 
         let entry = NatEntry {
@@ -77,15 +73,14 @@ impl AddressTranslator {
     }
 
     pub fn translate_inward(&mut self, data: &mut [u8]) -> Option<()> {
-        let (mut ipv4, _, rest) = ValidIpv4::parse(data).ok()?;
-        let ip_source = ipv4.source().into();
+        let mut ipv4 = Ipv4Packet::new_checked(data).ok()?;
+        let ip_source = ipv4.src_addr();
 
-        let key = Self::get_inward_key(ipv4.protocol(), rest, ip_source)?;
+        let key = Self::get_inward_key(ipv4.next_header(), ipv4.payload_mut(), ip_source)?;
         let entry = self.inward.get_mut(&key)?; // drop incoming packet without corresponding nat entry
 
         let new_state = Self::translate_packet(
             &mut ipv4,
-            rest,
             None,
             Some(*entry.internal.ip()),
             None,
@@ -98,28 +93,31 @@ impl AddressTranslator {
         Some(())
     }
 
-    fn get_inward_key(hint: IpProtocol, rest: &[u8], source: Ipv4Addr) -> Option<NatKey> {
-        Self::get_key(hint, rest, source, false, false)
+    fn get_inward_key(protocol: IpProtocol, payload: &[u8], source: Ipv4Addr) -> Option<NatKey> {
+        Self::get_key(protocol, payload, source, false, false)
     }
 
-    fn get_outward_key(hint: IpProtocol, rest: &[u8], source: Ipv4Addr) -> Option<NatKey> {
-        Self::get_key(hint, rest, source, true, true)
+    fn get_outward_key(protocol: IpProtocol, payload: &[u8], source: Ipv4Addr) -> Option<NatKey> {
+        Self::get_key(protocol, payload, source, true, true)
     }
 
     fn get_key(
-        hint: IpProtocol,
-        rest: &[u8],
+        protocol: IpProtocol,
+        payload: &[u8],
         source: Ipv4Addr,
         is_outward: bool,
         reversed: bool,
     ) -> Option<NatKey> {
-        match hint {
-            IpProtocol::TCP => {
-                let (tcp, ..) = ValidTcp::parse(rest).ok()?;
+        match protocol {
+            IpProtocol::Tcp => {
+                if payload.len() < TCP_HEADER_LEN {
+                    return None;
+                }
+                let tcp = TcpPacket::new_unchecked(payload);
                 let port = if reversed {
-                    tcp.source()
+                    tcp.src_port()
                 } else {
-                    tcp.destination()
+                    tcp.dst_port()
                 };
                 Some(NatKey::new(
                     Protocol::Tcp,
@@ -127,12 +125,15 @@ impl AddressTranslator {
                     is_outward.then_some(source),
                 ))
             }
-            IpProtocol::UDP => {
-                let (udp, ..) = ValidUdp::parse(rest).ok()?;
+            IpProtocol::Udp => {
+                if payload.len() < UDP_HEADER_LEN {
+                    return None;
+                }
+                let udp = UdpPacket::new_unchecked(payload);
                 let port = if reversed {
-                    udp.source()
+                    udp.src_port()
                 } else {
-                    udp.destination()
+                    udp.dst_port()
                 };
                 // if outward, provide originator - ensures correct NAT operation
                 // if inward, do not provide originator - this turns NAT from symmetric to full cone
@@ -142,32 +143,36 @@ impl AddressTranslator {
                     is_outward.then_some(source),
                 ))
             }
-            IpProtocol::ICMP => {
-                let (icmp, _, rest_icmp) = ValidIcmpV4::parse(rest).ok()?;
-                match icmp.ty() {
-                    ty if ty.payload_is_packet() => {
-                        let (ipv4, _, rest_ipv4) = ValidIpv4::parse(rest_icmp).ok()?;
-                        let original_destination = ipv4.destination().into();
+            IpProtocol::Icmp => {
+                let icmp = Icmpv4Packet::new_checked(payload).ok()?;
+                match icmp.msg_type() {
+                    Icmpv4Message::DstUnreachable
+                    | Icmpv4Message::Redirect
+                    | Icmpv4Message::TimeExceeded
+                    | Icmpv4Message::ParamProblem => {
+                        let icmp_payload = icmp.data();
+                        if icmp_payload.len() < IPV4_HEADER_LEN {
+                            return None;
+                        }
+
+                        let ipv4 = Ipv4Packet::new_unchecked(icmp_payload);
+                        ipv4.verify_checksum().then_some(())?;
                         Self::get_key(
-                            ipv4.protocol(),
-                            rest_ipv4,
-                            original_destination,
+                            ipv4.next_header(),
+                            ipv4.payload(),
+                            ipv4.dst_addr(),
                             is_outward,
                             !reversed,
                         )
                     }
-                    IcmpV4Type::ECHO_REQUEST
-                    | IcmpV4Type::ECHO_REPLY
-                    | IcmpV4Type::TIMESTAMP
-                    | IcmpV4Type::TIMESTAMP_REPLY => {
-                        let id_slice = &icmp.rest_of_hdr()[0..2];
-                        let id = u16::from_be_bytes(id_slice.try_into().unwrap());
-                        Some(NatKey::new(
-                            Protocol::Icmp,
-                            id,
-                            is_outward.then_some(source),
-                        ))
-                    }
+                    Icmpv4Message::EchoRequest
+                    | Icmpv4Message::EchoReply
+                    | Icmpv4Message::Timestamp
+                    | Icmpv4Message::TimestampReply => Some(NatKey::new(
+                        Protocol::Icmp,
+                        icmp.echo_ident(),
+                        is_outward.then_some(source),
+                    )),
                     _ => None,
                 }
             }
@@ -176,7 +181,10 @@ impl AddressTranslator {
     }
 
     fn get_free_external_port(&mut self, protocol: Protocol, wanted_port: u16) -> Option<u16> {
-        if !self.inward.contains_key(&NatKey::new(protocol, wanted_port, None)) {
+        if !self
+            .inward
+            .contains_key(&NatKey::new(protocol, wanted_port, None))
+        {
             return Some(wanted_port);
         }
 
@@ -187,7 +195,10 @@ impl AddressTranslator {
                 continue;
             }
 
-            if !self.inward.contains_key(&NatKey::new(protocol, wanted_port, None)) {
+            if !self
+                .inward
+                .contains_key(&NatKey::new(protocol, wanted_port, None))
+            {
                 return Some(wanted_port);
             }
 
@@ -198,82 +209,95 @@ impl AddressTranslator {
     }
 
     fn translate_packet(
-        ipv4: &mut ValidIpv4<&mut [u8]>,
-        rest: &mut [u8],
+        ipv4: &mut Ipv4Packet<&mut [u8]>,
         src: Option<Ipv4Addr>,
         dest: Option<Ipv4Addr>,
         src_port: Option<u16>,
         dest_port: Option<u16>,
     ) -> Option<ProtocolState> {
         if let Some(src) = src {
-            ipv4.set_source(src.into());
+            ipv4.set_src_addr(src);
         }
 
         if let Some(dest) = dest {
-            ipv4.set_destination(dest.into());
+            ipv4.set_dst_addr(dest);
         }
 
-        match ipv4.protocol() {
-            IpProtocol::TCP => {
-                let (mut tcp, ..) = ValidTcp::parse(rest).ok()?;
+        let src_addr = &ipv4.src_addr().into();
+        let dst_addr = &ipv4.dst_addr().into();
+
+        let new_state = match ipv4.next_header() {
+            IpProtocol::Tcp => {
+                let mut tcp = TcpPacket::new_unchecked(ipv4.payload_mut());
                 if let Some(src_port) = src_port {
-                    tcp.set_source(src_port);
+                    tcp.set_src_port(src_port);
                 }
                 if let Some(dest_port) = dest_port {
-                    tcp.set_destination(dest_port);
+                    tcp.set_dst_port(dest_port);
                 }
 
-                let flags = tcp.flags();
-                bitflags_match!(flags, {
-                    TcpFlags::SYN => ProtocolState::TcpSynSent,
-                    TcpFlags::SYN | TcpFlags::ACK => ProtocolState::TcpSynReceived,
-                    TcpFlags::FIN => ProtocolState::TcpClosing,
-                    TcpFlags::FIN | TcpFlags::ACK => ProtocolState::TcpClosing,
-                    TcpFlags::RST => ProtocolState::TcpClosed,
-                    _ => ProtocolState::TcpEstablished,
-                });
-                Some(ProtocolState::TcpEstablished)
+                tcp.fill_checksum(src_addr, dst_addr);
+                match tcp {
+                    _ if tcp.rst() => Some(ProtocolState::TcpClosed),
+                    _ if tcp.fin() && tcp.ack() => Some(ProtocolState::TcpClosing),
+                    _ if tcp.fin() => Some(ProtocolState::TcpClosing),
+                    _ if tcp.syn() && tcp.ack() => Some(ProtocolState::TcpSynReceived),
+                    _ if tcp.syn() => Some(ProtocolState::TcpSynSent),
+                    _ => Some(ProtocolState::TcpEstablished),
+                }
             }
-            IpProtocol::UDP => {
-                let (mut udp, ..) = ValidUdp::parse(rest).ok()?;
+            IpProtocol::Udp => {
+                let mut udp = UdpPacket::new_unchecked(ipv4.payload_mut());
                 if let Some(src_port) = src_port {
-                    udp.set_source(src_port);
+                    udp.set_src_port(src_port);
                 }
                 if let Some(dest_port) = dest_port {
-                    udp.set_destination(dest_port);
+                    udp.set_dst_port(dest_port);
                 }
+                udp.fill_checksum(src_addr, dst_addr);
                 Some(ProtocolState::Udp)
             }
-            IpProtocol::ICMP => {
-                let (mut icmp, _, rest_icmp) = ValidIcmpV4::parse(rest).ok()?;
-                match icmp.ty() {
-                    ty if ty.payload_is_packet() => {
-                        let (mut nested_ipv4, _, rest_nested) = ValidIpv4::parse(rest_icmp).ok()?;
+            IpProtocol::Icmp => {
+                let mut icmp = Icmpv4Packet::new_unchecked(ipv4.payload_mut());
+                match icmp.msg_type() {
+                    Icmpv4Message::DstUnreachable
+                    | Icmpv4Message::Redirect
+                    | Icmpv4Message::TimeExceeded
+                    | Icmpv4Message::ParamProblem => {
+                        let mut nested_ipv4 = Ipv4Packet::new_unchecked(icmp.data_mut());
                         // flipped source and destination arguments, as ICMP needs it
-                        Self::translate_packet(
+                        let result = Self::translate_packet(
                             &mut nested_ipv4,
-                            rest_nested,
                             dest,
                             src,
                             dest_port,
                             src_port,
-                        )
-                    }
-                    IcmpV4Type::ECHO_REQUEST
-                    | IcmpV4Type::ECHO_REPLY
-                    | IcmpV4Type::TIMESTAMP
-                    | IcmpV4Type::TIMESTAMP_REPLY => {
-                        if let Some(new_id) = src_port.or(dest_port) {
-                            let id_slice = u16::to_be_bytes(new_id);
-                            let rest_of_hdr = icmp.rest_of_hdr_mut();
-                            rest_of_hdr[0..2].copy_from_slice(&id_slice);
+                        );
+
+                        if result.is_some() {
+                            icmp.fill_checksum();
                         }
+                        result
+                    }
+                    Icmpv4Message::EchoRequest
+                    | Icmpv4Message::EchoReply
+                    | Icmpv4Message::Timestamp
+                    | Icmpv4Message::TimestampReply => {
+                        if let Some(new_id) = src_port.or(dest_port) {
+                            icmp.set_echo_ident(new_id);
+                        }
+                        icmp.fill_checksum();
                         Some(ProtocolState::Icmp)
                     }
                     _ => None,
                 }
             }
             _ => None,
+        };
+
+        if new_state.is_some() {
+            ipv4.fill_checksum();
         }
+        new_state
     }
 }
--- src/nat/test.rs
+++ src/nat/test.rs
@@ -1,10 +1,10 @@
 use std::net::Ipv4Addr;
 
-use ingot::icmp::{IcmpV4, IcmpV4Type};
-use ingot::ip::Ipv4;
-use ingot::types::Emit;
-use ingot::udp::Udp;
-use ingot::{ip::IpProtocol, tcp::Tcp};
+use smoltcp::phy::ChecksumCapabilities;
+use smoltcp::wire::{
+    Icmpv4DstUnreachable, Icmpv4Packet, Icmpv4Repr, Icmpv4TimeExceeded, IpProtocol, Ipv4Repr,
+    TcpControl, TcpPacket, TcpRepr, TcpSeqNumber, UdpPacket, UdpRepr,
+};
 
 use crate::nat::{AddressTranslator, NatKey, Protocol};
 
@@ -15,133 +15,166 @@ const ICMP_ID: u16 = 1357;
 const FAKE_IPV4_PKT_SRC: Ipv4Addr = Ipv4Addr::new(1, 2, 3, 4);
 const FAKE_IPV4_PKT_DST: Ipv4Addr = Ipv4Addr::new(5, 6, 7, 8);
 
-fn fake_ipv4_packet(protocol: IpProtocol) -> Ipv4 {
-    Ipv4 {
-        protocol,
-        source: FAKE_IPV4_PKT_SRC.into(),
-        destination: FAKE_IPV4_PKT_DST.into(),
-        ..Default::default()
+fn fake_ipv4_repr(protocol: IpProtocol, payload: &[u8]) -> Ipv4Repr {
+    Ipv4Repr {
+        src_addr: FAKE_IPV4_PKT_SRC,
+        dst_addr: FAKE_IPV4_PKT_DST,
+        next_header: protocol,
+        payload_len: payload.len(),
+        hop_limit: 64,
     }
 }
 
-fn fake_tcp_packet() -> Tcp {
-    Tcp {
-        source: SOURCE_PORT,
-        destination: DESTINATION_PORT,
-        ..Default::default()
-    }
+fn fake_tcp_packet() -> Vec<u8> {
+    let repr = TcpRepr {
+        src_port: SOURCE_PORT,
+        dst_port: DESTINATION_PORT,
+        control: TcpControl::None,
+        seq_number: TcpSeqNumber(1),
+        ack_number: None,
+        window_len: 64,
+        window_scale: None,
+        max_seg_size: None,
+        sack_permitted: false,
+        sack_ranges: [None; 3],
+        timestamp: None,
+        payload: &[],
+    };
+
+    let mut buffer = vec![0; repr.buffer_len()];
+    let mut packet = TcpPacket::new_unchecked(&mut buffer);
+    repr.emit(
+        &mut packet,
+        &FAKE_IPV4_PKT_SRC.into(),
+        &FAKE_IPV4_PKT_DST.into(),
+        &ChecksumCapabilities::default(),
+    );
+    buffer
 }
 
-fn fake_udp_packet() -> Udp {
-    Udp {
-        source: SOURCE_PORT,
-        destination: DESTINATION_PORT,
-        ..Default::default()
-    }
+fn fake_udp_packet() -> Vec<u8> {
+    let repr = UdpRepr {
+        src_port: SOURCE_PORT,
+        dst_port: DESTINATION_PORT,
+    };
+
+    let mut buffer = vec![0; repr.header_len()];
+    let mut packet = UdpPacket::new_unchecked(&mut buffer);
+    repr.emit(
+        &mut packet,
+        &FAKE_IPV4_PKT_SRC.into(),
+        &FAKE_IPV4_PKT_DST.into(),
+        0,
+        |_| (), // no payload
+        &ChecksumCapabilities::default(),
+    );
+    buffer
+}
+
+fn fake_icmp_packet(repr: Icmpv4Repr) -> Vec<u8> {
+    let mut buffer = vec![0; repr.buffer_len()];
+    let mut packet = Icmpv4Packet::new_unchecked(&mut buffer);
+    repr.emit(&mut packet, &ChecksumCapabilities::default());
+    buffer
 }
 
 #[test]
 fn inward_key_tcp() {
     let tcp = fake_tcp_packet();
-    let data = tcp.to_vec();
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_inward_key(IpProtocol::TCP, &data, address).unwrap();
+    let key = AddressTranslator::get_inward_key(IpProtocol::Tcp, &tcp, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Tcp, DESTINATION_PORT, None));
 }
 
 #[test]
 fn inward_key_udp() {
-    let udp = fake_tcp_packet();
-    let data = udp.to_vec();
+    let udp = fake_udp_packet();
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_inward_key(IpProtocol::UDP, &data, address).unwrap();
+    let key = AddressTranslator::get_inward_key(IpProtocol::Udp, &udp, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Udp, DESTINATION_PORT, None));
 }
 
 #[test]
 fn inward_key_icmp_echo() {
-    let id = u16::to_be_bytes(ICMP_ID);
-    let icmp = IcmpV4 {
-        ty: IcmpV4Type::ECHO_REPLY,
-        rest_of_hdr: [id[0], id[1], 0, 0],
-        code: 0,
-        checksum: 0,
+    let icmp = Icmpv4Repr::EchoReply {
+        ident: ICMP_ID,
+        seq_no: 1,
+        data: &[],
     };
 
-    let data = icmp.to_vec();
+    let data = fake_icmp_packet(icmp);
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_inward_key(IpProtocol::ICMP, &data, address).unwrap();
+    let key = AddressTranslator::get_inward_key(IpProtocol::Icmp, &data, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Icmp, ICMP_ID, None));
 }
 
 #[test]
 fn inward_key_icmp_error() {
-    let icmp = IcmpV4 {
-        ty: IcmpV4Type::TIME_EXCEEDED,
-        rest_of_hdr: [0, 0, 0, 0],
-        code: 0,
-        checksum: 0,
+    let udp_data = fake_udp_packet();
+    let icmp = Icmpv4Repr::TimeExceeded {
+        reason: Icmpv4TimeExceeded::TtlExpired,
+        header: fake_ipv4_repr(IpProtocol::Udp, &udp_data),
+        data: &udp_data,
     };
 
-    let data = (icmp, fake_ipv4_packet(IpProtocol::UDP), fake_udp_packet()).to_vec();
+    let data = fake_icmp_packet(icmp);
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_inward_key(IpProtocol::ICMP, &data, address).unwrap();
+    let key = AddressTranslator::get_inward_key(IpProtocol::Icmp, &data, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Udp, SOURCE_PORT, None));
 }
 
 #[test]
 fn outward_key_tcp() {
     let tcp = fake_tcp_packet();
-    let data = tcp.to_vec();
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_outward_key(IpProtocol::TCP, &data, address).unwrap();
+    let key = AddressTranslator::get_outward_key(IpProtocol::Tcp, &tcp, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Tcp, SOURCE_PORT, Some(address)));
 }
 
 #[test]
 fn outward_key_udp() {
-    let udp = fake_tcp_packet();
-    let data = udp.to_vec();
+    let udp = fake_udp_packet();
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_outward_key(IpProtocol::UDP, &data, address).unwrap();
+    let key = AddressTranslator::get_outward_key(IpProtocol::Udp, &udp, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Udp, SOURCE_PORT, Some(address)));
 }
 
 #[test]
 fn outward_key_icmp_echo() {
-    let id = u16::to_be_bytes(ICMP_ID);
-    let icmp = IcmpV4 {
-        ty: IcmpV4Type::ECHO_REQUEST,
-        rest_of_hdr: [id[0], id[1], 0, 0],
-        code: 0,
-        checksum: 0,
+    let icmp = Icmpv4Repr::EchoRequest {
+        ident: ICMP_ID,
+        seq_no: 1,
+        data: &[],
     };
 
-    let data = icmp.to_vec();
+    let data = fake_icmp_packet(icmp);
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_outward_key(IpProtocol::ICMP, &data, address).unwrap();
+    let key = AddressTranslator::get_outward_key(IpProtocol::Icmp, &data, address).unwrap();
     assert_eq!(key, NatKey::new(Protocol::Icmp, ICMP_ID, Some(address)));
 }
 
 #[test]
 fn outward_key_icmp_error() {
-    let icmp = IcmpV4 {
-        ty: IcmpV4Type::DESTINATION_UNREACHABLE,
-        rest_of_hdr: [0, 0, 0, 0],
-        code: 0,
-        checksum: 0,
+    let udp_data = fake_udp_packet();
+    let icmp = Icmpv4Repr::DstUnreachable {
+        reason: Icmpv4DstUnreachable::PortUnreachable,
+        header: fake_ipv4_repr(IpProtocol::Udp, &udp_data),
+        data: &udp_data,
     };
 
-    let data = (icmp, fake_ipv4_packet(IpProtocol::UDP), fake_udp_packet()).to_vec();
+    let data = fake_icmp_packet(icmp);
     let address = Ipv4Addr::UNSPECIFIED;
 
-    let key = AddressTranslator::get_outward_key(IpProtocol::ICMP, &data, address).unwrap();
-    assert_eq!(key, NatKey::new(Protocol::Udp, DESTINATION_PORT, Some(FAKE_IPV4_PKT_DST)));
+    let key = AddressTranslator::get_outward_key(IpProtocol::Icmp, &data, address).unwrap();
+    assert_eq!(
+        key,
+        NatKey::new(Protocol::Udp, DESTINATION_PORT, Some(FAKE_IPV4_PKT_DST))
+    );
 }
